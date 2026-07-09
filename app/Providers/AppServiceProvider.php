1edbaf0ce4adb9cc7752c7b3bcaad41fc25f38da
AbsenKID <itsdi@sankei-dharma.com>
2026-07-09 07:11:00 +0000

    Fix HTTPS mixed content + sync gambar doorprize ke cloud

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Setting;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS di production (Cloudflare reverse proxy)
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         // Make Setting model available in all Blade views
         View::share('Setting', new Setting());
     }
